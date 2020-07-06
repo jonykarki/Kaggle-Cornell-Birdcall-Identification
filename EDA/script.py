cd3ba6f0bd5d2df188133bee9572420085dbfa53
Jony <dev.jonykarki@gmail.com>
2020-07-06 02:40:31 +0000

    changed script

--- EDA/script.py
+++ EDA/script.py
@@ -1,19 +1,3 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-from IPython import get_ipython
-
-
-# %%
-import sys, os
-if os.path.abspath(os.pardir) not in sys.path:
-    sys.path.insert(1, os.path.abspath(os.pardir))
-import CONFIG
-get_ipython().magic(u'reload_ext autoreload')
-get_ipython().magic(u'autoreload 2')
-
-
-# %%
 import matplotlib.pyplot as plt 
 import os
 import numpy as np
@@ -21,45 +5,30 @@ import pandas as pd
 import librosa
 import librosa.display
 from librosa.core import resample, to_mono
-from tqdm.auto import tqdm
-import IPython.display as ipd
+import tqdm
 
 import concurrent.futures
 
+DATA_DIR = "/content/data"
 
-# %%
-DATA_DIR = CONFIG.CFG.DATA.BASE
-
-
-# %%
 train_df = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))[['ebird_code', 'filename', 'duration']]
 train_df.head()
 
-
-# %%
 train_df['filepath'] = ""
 train_df['filepath'] = train_df.apply(lambda row: os.path.join(DATA_DIR, "train_audio", row["ebird_code"], row["filename"]), axis=1)
 train_df.shape
 
-
-# %%
 train_sample = train_df[train_df['ebird_code'] == 'aldfly']
 train_sample.shape
 
-
-# %%
 SR = 32000
 D_TIME = 5 # take every 5 seconds
 D_SAMPLE = D_TIME * SR
 SAVE_FOLDER = "../out"
 
-
-# %%
 for bird in train_df['ebird_code'].unique():
     os.makedirs(os.path.join(SAVE_FOLDER, bird), exist_ok=True)
 
-
-# %%
 def save_as_npys(row):
     # read the file
     y, sr = librosa.load(row['filepath'], sr=SR)
@@ -93,93 +62,12 @@ def save_as_npys(row):
             np.save(file_name, sample_mel_spec)
 
 
-# %%
 rows_series_as_list = []
 for i, j in train_df.iterrows():
     rows_series_as_list.append(j)
 
-
-# %%
 from multiprocessing.pool import Pool
 
 
-# %%
 with Pool() as p:
-    r = list(tqdm(p.imap(save_as_npys, rows_series_as_list), total=len(rows_series_as_list)))
-
-
-# %%
-
-
-
-# %%
-plt.plot(y)
-
-
-# %%
-mel_spec = librosa.feature.melspectrogram(y=y, sr=SR)
-mel_spec = librosa.power_to_db(mel_spec, ref=np.max)
-
-
-# %%
-mel_spec.shape
-
-
-# %%
-def mono_to_color(X: np.ndarray,
-                  mean=None,
-                  std=None,
-                  norm_max=None,
-                  norm_min=None,
-                  eps=1e-6):
-    """
-    Code from https://www.kaggle.com/daisukelab/creating-fat2019-preprocessed-data
-    """
-    # Stack X as [X,X,X]
-    X = np.stack([X, X, X], axis=-1)
-
-    # Standardize
-    mean = mean or X.mean()
-    X = X - mean
-    std = std or X.std()
-    Xstd = X / (std + eps)
-    _min, _max = Xstd.min(), Xstd.max()
-    norm_max = norm_max or _max
-    norm_min = norm_min or _min
-    if (_max - _min) > eps:
-        # Normalize to [0, 255]
-        V = Xstd
-        V[V < norm_min] = norm_min
-        V[V > norm_max] = norm_max
-        V = 255 * (V - norm_min) / (norm_max - norm_min)
-        V = V.astype(np.uint8)
-    else:
-        # Just zero
-        V = np.zeros_like(Xstd, dtype=np.uint8)
-    return V
-
-
-# %%
-image = mono_to_color(mel_spec)
-plt.imshow(image)
-image.shape
-
-
-# %%
-# cowsth
-librosa.display.specshow(mel_spec, y_axis='mel')
-
-
-# %%
-# cowsth
-librosa.display.specshow(mel_spec, y_axis='mel')
-
-
-# %%
-# aldfly
-librosa.display.specshow(mel_spec, y_axis='mel')
-
-
-# %%
-
-
+    r = list(tqdm.tqdm(p.imap(save_as_npys, rows_series_as_list), total=len(rows_series_as_list)))
